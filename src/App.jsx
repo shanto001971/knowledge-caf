cf7479aa13bdaddaf988bb8615f740409b786fe1
Shanto Islam <rajs45693@gmail.com>
2023-03-30 21:21:03 +0600

    navbar ok

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Component/Header/Header';
+
+const App = () => {
+  return (
+    <div className='container mx-auto'>
+      <Header/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='p-4  flex justify-between'>
+                <h1 className='text-2xl font-bold'>
+                Knowledge Cafe
+                </h1>
+                <img className='w-10 rounded-full ' src="https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg" alt="" />
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
